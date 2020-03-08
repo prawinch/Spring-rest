1101c13cd2dc75c5d4bd83346ccf42dc6443cd62
praveen.cherukuri <praveen.cherukuri@hotmail.com>
2020-03-08 23:35:23 +0100

    Added request params to perform action based onthe input action

--- spring-rest-app/src/main/java/com/qam/app/demo/controller/MessageController.java
+++ spring-rest-app/src/main/java/com/qam/app/demo/controller/MessageController.java
@@ -11,10 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
-    @GetMapping(value = "/compute", produces = "application/json")
+ /*   @GetMapping(value = "/compute", produces = "application/json")
     public String greeting(@RequestParam String input) {
         String returnValue = StringUtils.reverse(input);
         String message = String.format("Entered text is : %s and the return value is %s", input, returnValue);
+        logger.info(message);
+        return message;
+    }
+*/
+
+    @GetMapping(value = "/compute")
+    public String textLength(@RequestParam(name = "input") String text, @RequestParam(name = "action", required = false, defaultValue = "length") String action) {
+
+        String returnValue;
+        switch (action) {
+            case "reverse":
+                returnValue = StringUtils.reverse(text);
+                break;
+            case "rotate":
+                returnValue = StringUtils.rotate(text, 3);
+                break;
+            default:
+                returnValue = String.valueOf(StringUtils.length(text));
+        }
+        String message = String.format("Entered text is : %s with action %s and the return value is %s", text, action, returnValue);
         logger.info(message);
         return message;
     }
